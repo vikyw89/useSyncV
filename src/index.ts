57907aeb6efa1421eeedb2120bc3d83efcdab33b
vikyw89 <vikyw89@gmail.com>
2023-05-04 17:37:39 +0800

    added set, get sync and asyncv

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 export { createSyncV } from './lib/createSyncV.js';
 export { deleteSyncV } from './lib/deleteSyncV.js';
+export { getSyncV } from './lib/getSyncV.js';
 export { debugSyncV } from './lib/helper.js';
 export { readSyncV } from './lib/readSyncV.js';
+export { setAsyncV } from './lib/setAsyncV.js';
+export { setSyncV } from './lib/setSyncV.js';
 export { updateAsyncV } from './lib/updateAsyncV.js';
 export { updateSyncV } from './lib/updateSyncV.js';
 export { useAsyncV } from './lib/useAsyncV.js';
 export { useQueryV } from './lib/useQueryV.js';
 export { useSyncV } from './lib/useSyncV.js';
+
+
+
+
